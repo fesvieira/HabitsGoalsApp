e25e245fb3621482ccfb872b243d5b674195f551
Felipe Silva Vieira <65823376+fesvieira@users.noreply.github.com>
2023-07-13 14:45:09 -0300

    (ignore) refactor

--- app/src/main/java/com/fesvieira/habitsgoals/ui/theme/Theme.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.fesvieira.habitsgoals.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
@@ -22,7 +21,7 @@ onSurface = Color.Black,
 */
 
 @Composable
-fun HabitsGoalsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun HabitsGoalsTheme(content: @Composable () -> Unit) {
     val colors = ColorPalette
 
     MaterialTheme(
