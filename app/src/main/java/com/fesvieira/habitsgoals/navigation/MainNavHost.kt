ece1de3a944f1abbee43a23afdf01282537e7b1d
fesvieira <felipe2204sv@gmail.com>
2023-08-28 21:35:57 -0300

    remove delay on splay screen

--- app/src/main/java/com/fesvieira/habitsgoals/navigation/MainNavHost.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/navigation/MainNavHost.kt
@@ -2,17 +2,14 @@ package com.fesvieira.habitsgoals.navigation
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.fesvieira.habitsgoals.navigation.Routes.EditHabit
 import com.fesvieira.habitsgoals.navigation.Routes.HabitList
-import com.fesvieira.habitsgoals.navigation.Routes.Splash
 import com.fesvieira.habitsgoals.ui.screens.EditCreateHabitScreen
 import com.fesvieira.habitsgoals.ui.screens.HabitListScreen
-import com.fesvieira.habitsgoals.ui.screens.SplashScreen
 import com.fesvieira.habitsgoals.viewmodel.HabitsViewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -23,12 +20,7 @@ fun MainNavHost() {
 
     val systemUiController = rememberSystemUiController()
 
-    NavHost(navController = navController, startDestination = Splash) {
-        composable(Splash) {
-            systemUiController.setSystemBarsColor(Color.Black)
-            SplashScreen(navController = navController)
-        }
-
+    NavHost(navController = navController, startDestination = HabitList) {
         composable(HabitList) {
             systemUiController.setSystemBarsColor(MaterialTheme.colors.primary)
             HabitListScreen(
--- app/src/main/java/com/fesvieira/habitsgoals/navigation/Routes.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/navigation/Routes.kt
@@ -1,7 +1,6 @@
 package com.fesvieira.habitsgoals.navigation
 
 object Routes {
-    const val Splash = "splash-screen"
     const val HabitList = "habit-list-screen"
     const val EditHabit = "edit-create-habit-screen"
 }
