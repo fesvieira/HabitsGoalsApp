9c38788cc3f89c38f68838367475f95e6b4f3836
felipe <felipe2204sv@gmail.com>
2023-09-06 15:49:12 -0300

    feature: create notification worker

--- app/src/main/java/com/fesvieira/habitsgoals/MainActivity.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.fesvieira.habitsgoals
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy.REPLACE
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.fesvieira.habitsgoals.helpers.NotifyWorker
+import com.fesvieira.habitsgoals.helpers.NotifyWorker.Companion.NOTIFICATION_ID
+import com.fesvieira.habitsgoals.helpers.NotifyWorker.Companion.WORK_NAME
 import com.fesvieira.habitsgoals.navigation.MainNavHost
 import com.fesvieira.habitsgoals.ui.theme.HabitsGoalsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -16,7 +25,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             MainScreen()
         }
+
+        val data = Data.Builder().putInt(NOTIFICATION_ID, 0).build()
+
+        scheduleNotification(
+            context = applicationContext,
+            delay = 1000,
+            data = data
+        )
     }
+}
+
+private fun scheduleNotification(context: Context, delay: Long, data: Data) {
+    val notificationWork = OneTimeWorkRequest
+        .Builder(NotifyWorker::class.java)
+        .setInitialDelay(delay, TimeUnit.MILLISECONDS)
+        .setInputData(data)
+        .build()
+
+    val instanceWorkManager = WorkManager.getInstance(context)
+    instanceWorkManager
+        .beginUniqueWork(WORK_NAME, REPLACE, notificationWork)
+        .enqueue()
 }
 
 @Composable
---
+++ app/src/main/java/com/fesvieira/habitsgoals/helpers/NotifyWorker.kt
@@ -0,0 +1,80 @@
+package com.fesvieira.habitsgoals.helpers
+
+import android.app.Notification.DEFAULT_ALL
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_HIGH
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.graphics.Color.RED
+import android.media.AudioAttributes
+import android.media.AudioAttributes.CONTENT_TYPE_SONIFICATION
+import android.media.AudioAttributes.USAGE_NOTIFICATION_RINGTONE
+import android.media.RingtoneManager.TYPE_NOTIFICATION
+import android.media.RingtoneManager.getDefaultUri
+import androidx.core.app.NotificationCompat
+import androidx.core.app.PendingIntentCompat.getActivity
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.fesvieira.habitsgoals.MainActivity
+import com.fesvieira.habitsgoals.R
+
+class NotifyWorker(context: Context, params: WorkerParameters): Worker(context, params) {
+
+    companion object {
+        const val NOTIFICATION_ID = "0"
+        const val NOTIFICATION_CHANNEL = "General"
+        const val NOTIFICATION_NAME = "Habit Reminder"
+        const val WORK_NAME = "Habit Reminder Work"
+    }
+    override fun doWork(): Result {
+        val id = inputData.getLong(NOTIFICATION_ID, 0).toInt()
+
+        sendNotification(id)
+        return Result.success()
+    }
+
+    private fun sendNotification(id: Int) {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK
+        intent.putExtra(NOTIFICATION_ID, id)
+
+        val notificationManager =
+            applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val pendingIntent =
+            getActivity(applicationContext, 0, intent, 0, false)
+
+        val notification = NotificationCompat
+            .Builder(applicationContext, NOTIFICATION_CHANNEL)
+            .setSmallIcon(R.drawable.ic_save)
+            .setContentTitle(NOTIFICATION_NAME)
+            .setContentText("testing notifications")
+            .setDefaults(DEFAULT_ALL)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        notification.priority = IMPORTANCE_HIGH
+
+        notification.setChannelId(NOTIFICATION_CHANNEL)
+
+        val ringtoneManager = getDefaultUri(TYPE_NOTIFICATION)
+        val audioAttributes = AudioAttributes.Builder().setUsage(USAGE_NOTIFICATION_RINGTONE)
+            .setContentType(CONTENT_TYPE_SONIFICATION).build()
+
+        val channel =
+            NotificationChannel(NOTIFICATION_CHANNEL, NOTIFICATION_CHANNEL, IMPORTANCE_HIGH)
+
+        channel.enableLights(true)
+        channel.lightColor = RED
+        channel.enableVibration(true)
+        channel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+        channel.setSound(ringtoneManager, audioAttributes)
+        notificationManager.createNotificationChannel(channel)
+
+        notificationManager.notify(id, notification.build())
+    }
+}
