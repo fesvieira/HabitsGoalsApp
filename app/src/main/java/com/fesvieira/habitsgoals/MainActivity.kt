e6a3f0d5ba9a9672679f475733527a417e06a877
felipe <felipe2204sv@gmail.com>
2023-09-07 17:47:34 -0300

    test: cancel periodic notifications

--- app/src/main/java/com/fesvieira/habitsgoals/MainActivity.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/MainActivity.kt
@@ -2,8 +2,10 @@ package com.fesvieira.habitsgoals
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.work.Data
@@ -17,6 +19,10 @@ import com.fesvieira.habitsgoals.helpers.NotifyWorker.Companion.WORK_NAME
 import com.fesvieira.habitsgoals.navigation.MainNavHost
 import com.fesvieira.habitsgoals.ui.theme.HabitsGoalsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
@@ -37,15 +43,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun scheduleNotification(context: Context, delay: Long, data: Data) {
+private fun scheduleNotification(
+    context: Context,
+    delay: Long,
+    data: Data
+) {
     val request =
         PeriodicWorkRequestBuilder<NotifyWorker>(15, TimeUnit.MINUTES)
             .setInitialDelay(delay, TimeUnit.MILLISECONDS)
             .setInputData(data)
+            .addTag(WORK_NAME)
             .build()
 
     val instanceWorkManager = WorkManager.getInstance(context)
     instanceWorkManager.enqueue(request)
+
+    CoroutineScope(Dispatchers.Default).launch {
+        delay(3000)
+        instanceWorkManager.cancelAllWorkByTag(WORK_NAME)
+        Log.d("Aoba", instanceWorkManager.getWorkInfosByTag(WORK_NAME).toString())
+    }
 }
 
 @Composable
