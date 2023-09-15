11a11efb1124ce2d07cf18cd1f9baced7d023fd4
felipe <felipe2204sv@gmail.com>
2023-09-15 12:36:41 -0300

    minor change

--- app/src/main/java/com/fesvieira/habitsgoals/helpers/isAllowedTo.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/helpers/isAllowedTo.kt
@@ -3,6 +3,6 @@ package com.fesvieira.habitsgoals.helpers
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
-
-fun Context.isAllowedTo(permission:String) =
+/** Use android.Manifest.permission to check */
+fun Context.isAllowedTo(permission: String) =
     ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
