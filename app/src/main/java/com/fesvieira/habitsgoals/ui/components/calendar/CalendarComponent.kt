830f54283be8bcbdb3167485cc6534d2dcd7ba2a
felipevieira <felipe2204sv@gmail.com>
2023-10-06 14:55:06 -0300

    enhance click animations in HabitDetailScreen

--- app/src/main/java/com/fesvieira/habitsgoals/ui/components/calendar/CalendarComponent.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/ui/components/calendar/CalendarComponent.kt
@@ -46,6 +46,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fesvieira.habitsgoals.R
+import com.fesvieira.habitsgoals.helpers.noRippleClickable
 import com.fesvieira.habitsgoals.helpers.toStamp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -149,7 +150,7 @@ fun CalendarComponent(
                         .size(24.dp)
                         .scale(leftArrowScaleAnim)
                         .clip(CircleShape)
-                        .clickable {
+                        .noRippleClickable {
                             coroutineScope.launch {
                                 dateShift -= 1
                                 leftArrowScale = 1.4f
@@ -169,7 +170,7 @@ fun CalendarComponent(
                         .size(24.dp)
                         .scale(rightArrowScaleAnim)
                         .clip(CircleShape)
-                        .clickable {
+                        .noRippleClickable {
                             coroutineScope.launch {
                                 dateShift += 1
                                 rightArrowScale = 1.4f
--- app/src/main/java/com/fesvieira/habitsgoals/ui/screens/HabitDetailScreen.kt
+++ app/src/main/java/com/fesvieira/habitsgoals/ui/screens/HabitDetailScreen.kt
@@ -1,63 +1,63 @@
 package com.fesvieira.habitsgoals.ui.screens
 
-import android.Manifest.permission.POST_NOTIFICATIONS
-import android.os.Build
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberTimePickerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.fesvieira.habitsgoals.R
-import com.fesvieira.habitsgoals.helpers.isAllowedTo
-import com.fesvieira.habitsgoals.helpers.minutesToHours
-import com.fesvieira.habitsgoals.ui.components.AppFloatActionButton
-import com.fesvieira.habitsgoals.ui.components.TopBar
-import com.fesvieira.habitsgoals.ui.components.calendar.CalendarComponent
-import com.fesvieira.habitsgoals.ui.components.dialogs.TimerPickerDialog
-import com.fesvieira.habitsgoals.ui.theme.Typography
-import com.fesvieira.habitsgoals.viewmodel.HabitsViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.time.LocalDate
+ import android.Manifest.permission.POST_NOTIFICATIONS
+ import android.os.Build
+ import android.widget.Toast
+ import androidx.activity.compose.rememberLauncherForActivityResult
+ import androidx.activity.result.contract.ActivityResultContracts
+ import androidx.compose.animation.AnimatedContent
+ import androidx.compose.animation.AnimatedVisibility
+ import androidx.compose.foundation.layout.Arrangement
+ import androidx.compose.foundation.layout.Row
+ import androidx.compose.foundation.layout.fillMaxSize
+ import androidx.compose.foundation.layout.fillMaxWidth
+ import androidx.compose.foundation.layout.padding
+ import androidx.compose.foundation.lazy.LazyColumn
+ import androidx.compose.foundation.shape.RoundedCornerShape
+ import androidx.compose.foundation.text.KeyboardOptions
+ import androidx.compose.material3.Checkbox
+ import androidx.compose.material3.CheckboxDefaults
+ import androidx.compose.material3.ExperimentalMaterial3Api
+ import androidx.compose.material3.MaterialTheme
+ import androidx.compose.material3.OutlinedTextField
+ import androidx.compose.material3.Scaffold
+ import androidx.compose.material3.Text
+ import androidx.compose.material3.rememberTimePickerState
+ import androidx.compose.runtime.Composable
+ import androidx.compose.runtime.LaunchedEffect
+ import androidx.compose.runtime.collectAsState
+ import androidx.compose.runtime.getValue
+ import androidx.compose.runtime.mutableStateOf
+ import androidx.compose.runtime.remember
+ import androidx.compose.runtime.rememberCoroutineScope
+ import androidx.compose.runtime.setValue
+ import androidx.compose.ui.Alignment
+ import androidx.compose.ui.ExperimentalComposeUiApi
+ import androidx.compose.ui.Modifier
+ import androidx.compose.ui.draw.clip
+ import androidx.compose.ui.graphics.Color
+ import androidx.compose.ui.platform.LocalContext
+ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+ import androidx.compose.ui.res.painterResource
+ import androidx.compose.ui.res.stringResource
+ import androidx.compose.ui.text.input.KeyboardType
+ import androidx.compose.ui.unit.dp
+ import androidx.compose.ui.unit.sp
+ import androidx.navigation.NavController
+ import com.fesvieira.habitsgoals.R
+ import com.fesvieira.habitsgoals.helpers.isAllowedTo
+ import com.fesvieira.habitsgoals.helpers.minutesToHours
+ import com.fesvieira.habitsgoals.helpers.noRippleClickable
+ import com.fesvieira.habitsgoals.ui.components.AppFloatActionButton
+ import com.fesvieira.habitsgoals.ui.components.TopBar
+ import com.fesvieira.habitsgoals.ui.components.calendar.CalendarComponent
+ import com.fesvieira.habitsgoals.ui.components.dialogs.TimerPickerDialog
+ import com.fesvieira.habitsgoals.ui.theme.Typography
+ import com.fesvieira.habitsgoals.viewmodel.HabitsViewModel
+ import kotlinx.coroutines.Dispatchers
+ import kotlinx.coroutines.delay
+ import kotlinx.coroutines.launch
+ import java.time.LocalDate
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -181,7 +181,7 @@ fun HabitDetailScreen(
                     modifier = Modifier
                         .padding(16.dp)
                         .clip(RoundedCornerShape(32.dp))
-                        .clickable {
+                        .noRippleClickable {
                             if (selectedHabit.reminder == null) {
                                 showTimePicker = true
                             } else {
